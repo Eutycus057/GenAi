9cf20f5013fc6092a598317eb91d65b3107258fd
Eutycus Mbuthia <162421713+Eutycus057@users.noreply.github.com>
2025-11-06 19:53:46 +0300

    Delete app.py

--- app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from jaclang_streamlit import run_streamlit
-
-if __name__ == "__main__":
-    st.set_page_config(page_title="MCP Chatbot", layout="wide")
-    run_streamlit("client", ".")
